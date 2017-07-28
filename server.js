e2914aacea1a76f92883154016dd3cc4bdc9b397
Brandyn Sullins <woofenwauzer@gmail.com>
2017-07-28 09:28:02 -0400

    todo done

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mustacheExpress = require('mustache-express')
 const app = express()
 const bodyParser = require('body-parser')
-
 var todos = []
 var completed = []
 
@@ -14,19 +13,23 @@ app.set('views', './views')
 app.set('view engine', 'mst')
 
 app.get('/', (req, res) => {
-  res.render('list', { todos: todos })
+  res.render('list', { todos: todos, completed: completed })
 })
 
-app.post('/', (req, res) => {
-  if (req.body.mark !== 'mark') {
-    todos.push(req.body.todo)
-  } else {
-    completed.push(req.body.todo)
-    console.log(completed)
+app.post('/add', (req, res) => {
+  todos.push(req.body.todo)
+  res.redirect('/')
+})
+
+app.post('/completed', (req, res) => {
+  var i = todos.indexOf(req.body.marked)
+  if (i != -1) {
+    todos.splice(i, 1)
+    completed.push(req.body.marked)
   }
   res.redirect('/')
 })
 
 app.listen(3000, (req, res) => {
-  console.log('Listening on dat 3k.')
+  console.log('Listening ft. Andre 3000')
 })
